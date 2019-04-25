211499abdc2c48726045f7bd5882056894cda742
jon <jedijon567@gmail.com>
2019-04-25 17:06:19 -0400

    it does a thing now

--- bootsec.asm
+++ bootsec.asm
@@ -2,6 +2,13 @@
 ; copyright EzioSoft 2019
 [org 0x7c00]
 
+; thing for bios teletype
+mov ah, 0x0e
+; put a letter in al
+mov al, 'f'
+; make bios call
+int 0x10
+
 loop:
 jmp loop
 ; fill empty space
