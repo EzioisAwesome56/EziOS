0a06ef4ed7298101b47c55e5a1864ae8e382eff3
jon <jedijon567@gmail.com>
2019-04-25 20:41:19 -0400

    start work on protected mode shit

--- stringpm.asm
+++ stringpm.asm
@@ -1,1 +1,27 @@
 [bits 32]
+; constants
+vram equ 0xb8000
+goodcolor equ 0x0f
+
+; prints a null terminated string
+printstring_pm:
+	pusha
+	mov edx, vram
+	
+printstring_pm_loop:
+	mov al, [ebx]
+	mov ah, goodcolor
+	
+	cmp al, 0 ; are we at end of string????
+	je done
+	
+	move [edx], ax
+	
+	add ebx, 1
+	add edx, 2
+	
+	jmp printstring_pm_loop
+	
+done:
+	popa
+	ret
